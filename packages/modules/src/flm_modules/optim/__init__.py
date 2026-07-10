070f2f64cdcbd193d89c8c020480f9411c71879e
Clouds <Clouds.Flowing@gmail.com>
2026-07-10 19:13:48 +0800

    feat: configure optimizer recipes (#5)
    * chore: bootstrap optimizer config PR
    
    * refactor: compose Muon with AdamW fallback
    
    * fix: include tensorboard in dev train extra

--- packages/modules/src/flm_modules/optim/__init__.py
+++ packages/modules/src/flm_modules/optim/__init__.py
@@ -1,9 +1,11 @@
 """Optimizer helpers."""
 
 from flm_modules.optim.adamw import configure_adamw
+from flm_modules.optim.composite import CompositeOptimizer
 from flm_modules.optim.muon import Muon, configure_muon
 
 __all__ = [
+  "CompositeOptimizer",
   "Muon",
   "configure_adamw",
   "configure_muon",
